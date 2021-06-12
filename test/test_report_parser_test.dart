ab5f9ffaa21f4af0e4f364b5503447ffc12f20bd
Renato Athaydes <renato@athaydes.com>
2021-06-12 20:21:14 +0200

    Implemented real-test-run test to make sure events are parsed correctly.

--- test/test_report_parser_test.dart
+++ test/test_report_parser_test.dart
@@ -12,7 +12,7 @@ void main() {
   test('basic test 1', () {});
   group('First Group', () {
     test('group 1 test 1', () {});
-    test('group 1 test 2', () {});
+    test('group 1 test 2', () {}, skip: true);
   });
   group('Second Group', () {
     test('group 2 test 1', () {});
@@ -38,10 +38,53 @@ void main() {
         name: 'dart run test',
         onStdoutLine: combine(parseJsonToEvent, events.add));
     expect(exitCode, equals(0));
-    expect(events[0],
-        isA<StartEvent>().having((e) => e.type, 'type', equals('start')));
-    expect(events[1],
-        isA<StartEvent>().having((e) => e.type, 'type', equals('start')));
+    emittedEvent<StartEvent>(events, (e) {
+      expect(e.type, equals('start'));
+      expect(e.protocolVersion, equals('0.1.1'));
+      expect(e.time, allOf(greaterThanOrEqualTo(0), lessThan(100)));
+    });
+    emittedEvent<SuiteEvent>(events, (e) {
+      expect(e.type, equals('suite'));
+      expect(e.suite.path, equals(tempTestFile.path));
+      expect(e.time, allOf(greaterThanOrEqualTo(0), lessThan(100)));
+    });
+    emittedEvent<AllSuitesEvent>(events, (e) {
+      expect(e.type, equals('allSuites'));
+      expect(e.count, equals(1));
+      expect(e.time, allOf(greaterThanOrEqualTo(0), lessThan(1000)));
+    });
+    emittedEvent<DoneEvent>(events, (e) {
+      expect(e.type, equals('done'));
+      expect(e.success, isTrue);
+      expect(e.time, allOf(greaterThanOrEqualTo(0), lessThan(10000)));
+    });
+    emittedEvents<GroupEvent>(events, (e) {
+      expect(e.map((a) => a.type).toList(),
+          equals(Iterable.generate(3, (_) => 'group').toList()));
+      expect(e.map((a) => a.group.name),
+          equals(['', 'First Group', 'Second Group']));
+      expect(e.map((a) => a.group.testCount), equals([4, 2, 1]));
+      expect(e.map((a) => a.group.parentID),
+          equals([isNull, isNotNull, isNotNull]));
+      expect(e.map((a) => '${a.group.line}:${a.group.column}'),
+          equals([equals('null:null'), equals('5:3'), equals('9:3')]));
+    });
+    emittedEvents<TestStartEvent>(events, (e) {
+      expect(e.map((a) => a.type).toList(),
+          equals(Iterable.generate(5, (_) => 'testStart').toList()));
+      expect(e.map((a) => a.test.id).toList(), equals([1, 3, 5, 6, 8]));
+      expect(
+          e.map((a) => a.test.name).toList(),
+          equals([
+            'loading ${tempTestFile.path}',
+            'basic test 1',
+            'First Group group 1 test 1',
+            'First Group group 1 test 2',
+            'Second Group group 2 test 1'
+          ]));
+      expect(e.map((a) => a.test.metadata.skip).toList(),
+          equals([false, false, false, true, false]));
+    });
   });
 
   test('can parse JSON events', () {
@@ -51,9 +94,34 @@ void main() {
   });
 }
 
-B Function(String) combine<A, B>(A Function(String) fun, B Function(A) other) {
+C Function(A) combine<A, B, C>(B Function(A) first, C Function(B) second) {
   return (input) {
     print(input);
-    return other(fun(input));
+    return second(first(input));
   };
+}
+
+void emittedEvent<T extends Event>(
+  List<Event> events,
+  void Function(T) assertions,
+) {
+  final event = events.whereType<T>();
+  if (event.isEmpty) {
+    throw Exception('Did not emit event $T');
+  }
+  if (event.length > 1) {
+    throw Exception('Emitted more than one event $T: $event');
+  }
+  assertions(event.first);
+}
+
+void emittedEvents<T extends Event>(
+  List<Event> events,
+  void Function(List<T>) assertions,
+) {
+  final event = events.whereType<T>().toList();
+  if (event.isEmpty) {
+    throw Exception('Did not emit event $T');
+  }
+  assertions(event);
 }
